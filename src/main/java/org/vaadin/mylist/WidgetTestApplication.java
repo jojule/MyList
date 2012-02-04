56ac906f9cc6442e0817eb0cc945eee023ff9001
Joonas Lehtinen <jole@jole.fi>
2012-02-04 19:08:48 +0200

    Initial commit

---
+++ src/main/java/org/vaadin/mylist/WidgetTestApplication.java
@@ -0,0 +1,23 @@
+package org.vaadin.mylist;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Window;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+public class WidgetTestApplication extends Application
+{
+    private Window window;
+
+    @Override
+    public void init()
+    {
+        window = new Window("Widget Test");
+        setMainWindow(window);
+        window.addComponent(new MyList());
+    }
+    
+}
